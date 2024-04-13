8445a38337899a685688eeefae88a586c556879e
Ehsanullah Haidary <ehsanullahhaidary@gmail.com>
2024-04-13 15:28:51 +0430

    project started

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  firebaseConfig: {
+    apiKey: "AIzaSyC1dFqjccZDRU_XzGu25myiiVkD3k-9AF8",
+    authDomain: "typescript--sign-in.firebaseapp.com",
+    projectId: "typescript--sign-in",
+    storageBucket: "typescript--sign-in.appspot.com",
+    messagingSenderId: "259053043429",
+    appId: "1:259053043429:web:b0e8005eec7e55939bde0c",
+  },
+};
